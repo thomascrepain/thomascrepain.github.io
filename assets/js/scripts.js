4633501dab302fc1d018cdbf721c0ee5f30d375f
Thomas Crepain <info@thomascrepain.be>
2023-06-15 21:49:45 +0200

    Install GA4 tag

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,19 +20,6 @@ Author URL: trendytheme.net
     = Google Analytics
 */
 
-/*
- * Google analytics
- */
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-17734313-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
 jQuery(function ($) {
 
     'use strict';
@@ -250,22 +237,6 @@ jQuery(function ($) {
             });
         }
 
-    });
-
-
-    /*
-     * Handle external links
-     * This opens every link with rel 'external' in a new window and tracks these clicks with Google analytics
-     */
-    $("a[rel='external']").click(function(ev) {
-        ev.preventDefault();
-
-        // track external link with Google Analytics
-        _gaq.push(['_trackPageview', this.href]);
-
-        // external links open in a new window (HTML Strict)
-        window.open(this.href);
-        return false;
     });
 });
 
